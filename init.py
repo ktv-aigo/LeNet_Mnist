07faeaebbb1e197f5a632ed0d8665c6f9791e527
khoatranrb <khoamata20002@gmail.com>
2020-01-07 16:07:07 +0700

    update

--- init.py
+++ init.py
@@ -24,38 +24,40 @@ X_train, X_test, Y_train, Y_test = train_test_split(
 
 model = model.LeNet.build((28, 28, 1))
 
-sgd = SGD(nesterov=True)
+model.summary()
 
-log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-tensorboard_callback = tf.keras.callbacks.TensorBoard(
-    log_dir=log_dir, histogram_freq=1)
+# sgd = SGD(nesterov=True)
 
-model.compile(loss="categorical_crossentropy",
-              optimizer=sgd, metrics=["accuracy"])
+# log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+# tensorboard_callback = tf.keras.callbacks.TensorBoard(
+#     log_dir=log_dir, histogram_freq=1)
 
-H = model.fit(X_train, Y_train, validation_data=(X_test, Y_test),
-              batch_size=128, epochs=10, verbose=1, callbacks=[tensorboard_callback])
+# model.compile(loss="categorical_crossentropy",
+#               optimizer=sgd, metrics=["accuracy"])
 
-predictions = model.predict(X_test, batch_size=128)
-print(classification_report(Y_test.argmax(axis=1),
-                            predictions.argmax(axis=1),
-                            target_names=[str(x) for x in le.classes_]))
+# H = model.fit(X_train, Y_train, validation_data=(X_test, Y_test),
+#               batch_size=128, epochs=10, verbose=1, callbacks=[tensorboard_callback])
+
+# predictions = model.predict(X_test, batch_size=128)
+# print(classification_report(Y_test.argmax(axis=1),
+#                             predictions.argmax(axis=1),
+#                             target_names=[str(x) for x in le.classes_]))
 
 
-plt.style.use("ggplot")
-plt.figure()
-plt.plot(np.arange(0, 20), H.history["loss"], label="train_loss")
-plt.plot(np.arange(0, 20), H.history["val_loss"], label="val_loss")
-plt.plot(np.arange(0, 20), H.history["acc"], label="train_acc")
-plt.plot(np.arange(0, 20), H.history["val_acc"], label="val_acc")
-plt.title("Training Loss and Accuracy")
-plt.xlabel("Epoch #")
-plt.ylabel("Loss/Accuracy")
-plt.legend()
-plt.show()
+# plt.style.use("ggplot")
+# plt.figure()
+# plt.plot(np.arange(0, 20), H.history["loss"], label="train_loss")
+# plt.plot(np.arange(0, 20), H.history["val_loss"], label="val_loss")
+# plt.plot(np.arange(0, 20), H.history["acc"], label="train_acc")
+# plt.plot(np.arange(0, 20), H.history["val_acc"], label="val_acc")
+# plt.title("Training Loss and Accuracy")
+# plt.xlabel("Epoch #")
+# plt.ylabel("Loss/Accuracy")
+# plt.legend()
+# plt.show()
 
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
+# model_json = model.to_json()
+# with open("model.json", "w") as json_file:
+#     json_file.write(model_json)
 
-model.save_weights("model.h5")
+# model.save_weights("model.h5")
